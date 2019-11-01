397f36078e4fbdf68b1243a15145533dc900f998
James Duffey <jamesduffey1997@gmail.com>
2019-11-01 16:48:02 +0200

    updated

--- index.php
+++ index.php
@@ -15,21 +15,19 @@ session_start();
        <h1><a href="index.php">Camagru</a><h1>
          <ul style="list-style-type:none; margin: 0; display: inline; padding: 0;">
          <li style="display: inline; float: left;"><a style="float:right;"href="users/register.php" style="margin-left:20px;">Register</a></li>
-         <script>
-             if ($_SESSION) {
+         <!-- <script> -->
+             <!-- if ($_SESSION) { -->
                 <li style="display: inline; float: left;"><a href='index.php?session_status=logout' style="margin-left:20px;">Log Out</a><li>
-             }
-         </script>
+             <!-- } -->
+         <!-- </script> -->
         </ul>
      </header>
     <section>
             <div class="reg">
              <h1 style="color: aqua; font-size:40px;">Welcome To Camagru</h1>
-                <?PHP
-                if ($_SESSION){
-                 echo '<script><a href="users/login.php" id="login_but">Login</a></script>';
-                }
-             ?>
+
+                <a href="users/login.php" id="login_but">Login</a>
+
                 </div>
     </section>
  <footer></footer>
