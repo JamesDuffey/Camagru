f38bd467baac6ae01b101f900e3ea2c11087ad84
James Duffey <jamesduffey1997@gmail.com>
2019-11-11 17:52:30 +0200

    moved gallery from index to seperate page

--- index.php
+++ index.php
@@ -14,25 +14,23 @@ session_start();
          
        <h1><a href="index.php">Camagru</a><h1>
          <ul style="list-style-type:none; margin: 0; display: inline; padding: 0;">
+         <li style="display: inline; float: left; padding-right: 20px;"><a style="float:right;" href="users/gallery.php" style="margin-left:20px;">Gallery</a></li>
             <?PHP
                 if (isset($_SESSION['user_id'])){
                     echo '<li style="display: inline; float: left; margin-right:20px;"><a style="float:right;"href="users/user_account.php" style="margin-left:20px;">Profile</a></li>';
                 }
-            ?>
-            <?PHP
+            
                 if (!isset($_SESSION['user_id'])){
                     echo '<li style="display: inline; float: left;"><a style="float:right;"href="users/register.php" style="margin-left:20px;">Register</a></li>';
                 }
-            ?>
-            <?PHP
+            
                 if (isset($_SESSION['user_id'])){
-                    echo '<li style="display: inline; float: left;"><a style="float:right;"href="users/upload_img.php" style="margin-left:20px;">Upload</a></li>';
+                    echo '<li style="display: inline; float: left; padding-right: 20px;"><a style="float:right;"href="users/upload_img.php" style="margin-left:20px;">Upload</a></li>';
                 }
-            ?>
-             <?PHP
-             if (isset($_SESSION['user_id'])) {
-                echo '<li style="display: inline; float: left;"><a href="index.php?session_status=logout" style="margin-left:20px;">Log Out</a><li>';
-              }
+            
+                if (isset($_SESSION['user_id'])) {
+                    echo '<li style="display: inline; float: left;"><a style="float:right;" href="index.php?session_status=logout" style="margin-left:20px;">Log Out</a><li>';
+                }
              ?>
         </ul>
      </header>
@@ -45,11 +43,10 @@ session_start();
                     echo '<a href="users/login.php" id="login_but">Login</a>';
                     }
                 else {
-                    include "functions/get_gallery.php";
-                    get_gallery();
+                    echo '<h1 style="color: aqua; font-size:40px;">Welcome '.$_SESSION['username'].'</h1>';
                 }
                 ?>
-                </div>
+            </div>
     </section>
  <footer></footer>
 </body>
