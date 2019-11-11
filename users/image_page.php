9b2300278476ef3a91401af7c251fbf9505d5574
James Duffey <jamesduffey1997@gmail.com>
2019-11-11 17:59:17 +0200

    added form for comments/likes/delete post

--- users/image_page.php
+++ users/image_page.php
@@ -14,24 +14,25 @@ session_start();
          
        <h1><a href="../index.php">Camagru</a><h1>
          <ul style="list-style-type:none; margin: 0; display: inline; padding: 0;">
+         <li style="display: inline; float: left; padding-right: 20px;"><a style="float:right;" href="gallery.php" style="margin-left:20px;">Gallery</a></li>
             <?PHP
                 if (isset($_SESSION['user_id'])){
-                    echo '<li style="display: inline; float: left; margin-right:20px;"><a style="float:right;"href="users/user_account.php" style="margin-left:20px;">Profile</a></li>';
+                    echo '<li style="display: inline; float: left; margin-right:20px;"><a style="float:right;"href="user_account.php" style="margin-left:20px;">Profile</a></li>';
                 }
             ?>
             <?PHP
                 if (!isset($_SESSION['user_id'])){
-                    echo '<li style="display: inline; float: left;"><a style="float:right;"href="users/register.php" style="margin-left:20px;">Register</a></li>';
+                    echo '<li style="display: inline; float: left;"><a style="float:right;"href="register.php" style="margin-left:20px;">Register</a></li>';
                 }
             ?>
             <?PHP
                 if (isset($_SESSION['user_id'])){
-                    echo '<li style="display: inline; float: left;"><a style="float:right;"href="users/upload_img.php" style="margin-left:20px;">Upload</a></li>';
+                    echo '<li style="display: inline; float: left;"><a style="float:right;"href="upload_img.php" style="margin-left:20px;">Upload</a></li>';
                 }
             ?>
              <?PHP
              if (isset($_SESSION['user_id'])) {
-                echo '<li style="display: inline; float: left;"><a href="index.php?session_status=logout" style="margin-left:20px;">Log Out</a><li>';
+                echo '<li style="display: inline; float: left;"><a href="../index.php?session_status=logout" style="margin-left:20px;">Log Out</a><li>';
               }
              ?>
         </ul>
@@ -39,9 +40,44 @@ session_start();
     <section>
             <div class="reg">
              <?PHP
-             include '../functions/get_img.php';
+                include '../functions/get_img.php';
+                include '../functions/delete_img.php';
+
+                // if (isset($_POST['like'])) {
+                //     post_like($_GET['img']);
+                // }
+                // if (isset($_POST['apathy'])) {
+                //     delete_like($_GET['img']);
+                // }
+                // if (isset($_POST['comment'])) {
+                //     post_comment($_GET['img']);
+                // }
+                if (isset($_POST['delete_post'])) {
+                    delete_img($_GET['img']);
+                }
                 get_image($_GET['img']);
             ?>
+            <form method="POST">
+                                <?php
+                                include '../functions/like_funcs.php';
+                                include '../functions/check_user_id.php';
+								echo "<div class='level'>";
+									if (!post_is_liked($_GET['img'])) {
+										echo "<div class='level-left'><input class='button is-light' type='submit' name='like' value='Like'></div><br/>";
+									} else {
+										echo "<div class='level-left'><input class='button is-success' type='submit' name='apathy' value='Liked'></div><br/>";
+									}
+									if (check_user_id($_GET['img'])) {
+										echo "<div class='level-right'><input class='button is-danger' type='submit' name='delete_post' value='Delete'></div>";
+									}
+								echo "</div>";
+								?>
+								<input class="textarea" type="text" name="cmntContent" placeholder="Leave a comment..">
+								<div class="field is-grouped is-grouped-right">
+                                    <br/>
+									<input class="button is-success" type="submit" name="comment" value="Comment">
+								</div>
+							</form>
             </div>
     </section>
  <footer></footer>
