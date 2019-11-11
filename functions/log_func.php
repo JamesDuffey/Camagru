984e73f15ce12429cd388df8fe282de975ee9f5e
James Duffey <jamesduffey1997@gmail.com>
2019-11-11 17:39:24 +0200

    added check for if user is verified or not

--- functions/log_func.php
+++ functions/log_func.php
@@ -6,16 +6,19 @@
         $get_data = $con->prepare("SELECT * FROM users WHERE username=:username");
         $get_data->execute(['username' => $u_username]);
         $user_data = $get_data->fetch();
-        if (empty($user_data) || $u_username != $user_data['username'] || $u_pass != $user_data['userpass']) {
+        if ($user_data['verified'] == 0){
+            echo "<script>window.alert('Your account has not been verfied yet! Please check your email to verify your account!')</script>";
+        }
+        else if (empty($user_data) || $u_username != $user_data['username'] || $u_pass != $user_data['userpass']) {
             echo "<script>window.alert('Incorrect username or password!')</script>";
         }
 
-        if ($u_username == $user_data['username'] && $u_pass == $user_data['userpass']) {
+        else if ($u_username == $user_data['username'] && $u_pass == $user_data['userpass']) {
             $_SESSION['email'] = $user_data['email'];
             $_SESSION['user_id'] = $user_data['user_id'];
             $_SESSION['username'] = $u_username;
             echo "<script>window.alert('Logged In')</script>";
-            echo "<script>window.location.replace('user_account.php')</script>";
+            echo "<script>window.location.replace('../index.php')</script>";
         }
     }
     $con = null;
