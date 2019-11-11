f94e243f362e43c826e0b928ad6429afe0cb5f61
James Duffey <jamesduffey1997@gmail.com>
2019-11-11 17:58:43 +0200

    added check if user has liked image before

---
+++ functions/like_funcs.php
@@ -0,0 +1,16 @@
+<?PHP
+function post_is_liked($img_id) {
+	include '../includes/connection.php';
+	if (isset($_SESSION['user_id'])) {
+		$like_usr_id = $_SESSION['user_id'];
+		$is_liked_sql = "SELECT * FROM likes WHERE l_uid=:usr_id AND l_img_id=:img_id ";
+		$is_liked = $con->prepare($is_liked_sql);
+		$is_liked->execute(array(':usr_id'=>$like_usr_id, ':img_id'=>$img_id));
+		$usr_likes = $is_liked->fetchAll();
+		$con = null;
+		if (!empty($usr_likes))
+			return true;
+	}
+	return false;
+}
+?>
