e14b31daa24836fef5db1d2ad7fa70b1c93f8f38
James Duffey <jamesduffey1997@gmail.com>
2019-11-15 15:59:10 +0200

    added neccesary files for functions

--- functions/change_acc.php
+++ functions/change_acc.php
@@ -1,14 +1,18 @@
 <?PHP
 function update_user($user_id) {
+	include_once 'update_acc.php';
 	if (isset($_POST['username'])) {
+		include_once 'validation.php';
 		validate_name($_POST['username']);
 		update_username($user_id, $_POST['userame']);
 	}
 	if (isset($_POST['email'])) {
+		include_once 'validation.php';
 		validate_password($_POST['new_pass']);
 		update_email($user_id, $_POST['new_email']);
 	}
 	if (isset($_POST['new_pass'])) {
+		include 'validation.php';
 		validate_password($_POST['new_pass']);
 		update_pass($user_id, hash('whirlpool',$_POST['new_pass']));
 	}
